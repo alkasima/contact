3901695989e92996e5848743b3291bf1d223dd62
Kasim Abubakar Jajere <alkasima@gmail.com>
2022-05-03 09:21:22 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class ContactList extends React.Component {
+  render() {
+    const people = [
+      {name: 'Micheal'},
+      {name: 'Ryan'},
+      {name: 'Tyler'}
+    ]
+
+    return <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+
+  }
+}
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList/>
+      <ContactList/>
+      <ContactList/>
     </div>
   );
 }
